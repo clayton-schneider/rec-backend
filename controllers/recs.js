980fa4bd70fa702d12221176aeaf8507444b48e7
Clayton <schneiderc2016@gmail.com>
2020-04-18 12:45:06 -0400

    A freaking typo

--- controllers/recs.js
+++ controllers/recs.js
@@ -1,4 +1,4 @@
-const ErrorResponse = require('../utils/ErrorResponse');
+const ErrorResponse = require('../utils/errorResponse');
 const asyncHandler = require('../middleware/async');
 const Rec = require('../models/Rec');
 const User = require('../models/User');
