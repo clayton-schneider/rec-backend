41f78ddc6a8d30d4c495d1e87ababa0aa3da69c4
Clayton <schneiderc2016@gmail.com>
2020-09-07 13:18:05 -0400

    test for fix

--- middleware/auth.js
+++ middleware/auth.js
@@ -3,6 +3,7 @@ const ErrorResponse = require('../utils/errorResponse');
 
 // Protect routes
 exports.protect = asyncHandler(async (req, res, next) => {
+  console.log(req.user);
   if (!req.user)
     return next(new ErrorResponse('Not authorized to access this route', 401));
   next();
